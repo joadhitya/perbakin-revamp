3f2732dccee48efa784c59c895f51728aa701ba4
joadhitya <37260522+joadhitya@users.noreply.github.com>
2024-05-10 14:20:18 +0700

    [Migrations] Initiate Master Data Migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,28 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('username')->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->integer('place_birth')->nullable();
+            $table->date('date_birth')->nullable();
+            $table->string('sex')->nullable();
+            $table->integer('id_role')->default(1);
+            $table->string('role_name')->nullable();
+            $table->integer('id_position')->default(1);
+            $table->string('position_name')->nullable();
+            $table->integer('is_active')->default(1);
+            $table->integer('id_periode')->default(0);
+            $table->string('is_first_login')->default('Y');
+            $table->integer('count_error_login')->default(0);
+            $table->string('bg_color')->nullable();
+            $table->string('forgot_passcode')->nullable();
+            $table->datetime('forgot_passcode_valid_to')->nullable();
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
